562a8d751cf43d389d86cdd2401381927a69044b
David Berkman <david.berkman@grenadefish.net>
2018-08-03 08:50:14 -0700

    work

--- phalanx/src/main/java/org/smallmind/phalanx/blockchain/Block.java
+++ phalanx/src/main/java/org/smallmind/phalanx/blockchain/Block.java
@@ -78,14 +78,8 @@ public class Block<D extends Data> {
     return hash;
   }
 
-  public void mineBlock (int difficulty)
-    throws NoSuchAlgorithmException {
+  public void mineBlock () {
 
-    String target = new String(new char[difficulty]).replace('\0', '0'); //Create a string with difficulty * "0"
-    while (!hash.substring(0, difficulty).equals(target)) {
-      nonce++;
-      hash = calculateHash();
-    }
     System.out.println("Block Mined!!! : " + hash);
   }
 }
